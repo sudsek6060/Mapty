fbb22d2f76eefcd8abf20b4b82994284ecad9430
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-03-19 06:41:29 +0530

    targeting Elements

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
