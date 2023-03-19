2217b056a73be798985dd82578a0bd69ad1d220d
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-03-19 18:52:12 +0530

    load map using leaflate

--- script.js
+++ script.js
@@ -8,13 +8,25 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 
 if (navigator.geolocation)
-      navigator.geolocation.getCurrentPosition(
-        function (position){
-            const {latitude} = position.coords;
-            const {longitude} = position.coords;
+    navigator.geolocation.getCurrentPosition(
+        function (position) {
+            const { latitude } = position.coords;
+            const { longitude } = position.coords;
             console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+
+            const coords = [latitude, longitude]
+
+            const map = L.map('map').setView(coords, 13);
+
+            L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            L.marker(coords).addTo(map)
+                .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+                .openPopup();
         },
         function () {
-          alert('Could not get your position');
+            alert('Could not get your position');
         }
-      );
+    );
