dbd3c417fe6711c0a8e1ba517c8a582346847537
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-03-20 22:01:47 +0530

    validating form inputs

--- script.js
+++ script.js
@@ -9,45 +9,45 @@ const inputElevation = document.querySelector('.form__input--elevation');
 class Workout {
     date = new Date();
     id = (Date.now() + '').slice(-10);
-  
+
     constructor(coords, distance, duration) {
-      this.coords = coords; // [lat, lng]
-      this.distance = distance; // in km
-      this.duration = duration; // in min
+        this.coords = coords; // [lat, lng]
+        this.distance = distance; // in km
+        this.duration = duration; // in min
     }
-  }
-  
-  class Running extends Workout {
+}
+
+class Running extends Workout {
     type = 'running';
-  
+
     constructor(coords, distance, duration, cadence) {
-      super(coords, distance, duration);
-      this.cadence = cadence;
-      this.calcPace();
+        super(coords, distance, duration);
+        this.cadence = cadence;
+        this.calcPace();
     }
-  
+
     calcPace() {
-      // min/km
-      this.pace = this.duration / this.distance;
-      return this.pace;
+        // min/km
+        this.pace = this.duration / this.distance;
+        return this.pace;
     }
-  }
-  
-  class Cycling extends Workout {
+}
+
+class Cycling extends Workout {
     type = 'cycling';
-  
+
     constructor(coords, distance, duration, elevationGain) {
-      super(coords, distance, duration);
-      this.elevationGain = elevationGain;
-      this.calcSpeed();
+        super(coords, distance, duration);
+        this.elevationGain = elevationGain;
+        this.calcSpeed();
     }
-  
+
     calcSpeed() {
-      // km/h
-      this.speed = this.distance / (this.duration / 60);
-      return this.speed;
+        // km/h
+        this.speed = this.distance / (this.duration / 60);
+        return this.speed;
     }
-  }
+}
 
 class App {
     #map;
@@ -80,7 +80,7 @@ class App {
         L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(this.#map);
-        this.#map.on('click', this._showForm.bind(this) )
+        this.#map.on('click', this._showForm.bind(this))
 
     }
 
@@ -97,9 +97,47 @@ class App {
     _toggleElevationField() {
         inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
         inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-      }
+    }
     _newWorkout(e) {
-        e.preventDefault()
+        const validInputs = (...inputs) =>
+            inputs.every(inp => Number.isFinite(inp));
+        const allPositive = (...inputs) => inputs.every(inp => inp > 0);
+
+        e.preventDefault();
+
+        // Get data from form
+        const type = inputType.value;
+        const distance = +inputDistance.value;
+        const duration = +inputDuration.value;
+
+
+        // If workout running, create running object
+        if (type === 'running') {
+            const cadence = +inputCadence.value;
+
+            // Check if data is valid
+            if (
+                !validInputs(distance, duration, cadence) ||
+                !allPositive(distance, duration, cadence)
+            )
+                return alert('Inputs have to be positive numbers!');
+
+
+        }
+
+        // If workout cycling, create cycling object
+        if (type === 'cycling') {
+            const elevation = +inputElevation.value;
+
+            if (
+                !validInputs(distance, duration, elevation) ||
+                !allPositive(distance, duration)
+            )
+                return alert('Inputs have to be positive numbers!');
+
+
+        }
+
         inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
 
         const { lat, lng } = this.#mapEvent.latlng;
