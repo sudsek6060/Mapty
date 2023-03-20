0cf6d3a7c10474604010eaf6c25e7a5152178969
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-03-20 20:09:46 +0530

    workout class

--- script.js
+++ script.js
@@ -6,7 +6,48 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-// let map, mapEvent;
+class Workout {
+    date = new Date();
+    id = (Date.now() + '').slice(-10);
+  
+    constructor(coords, distance, duration) {
+      this.coords = coords; // [lat, lng]
+      this.distance = distance; // in km
+      this.duration = duration; // in min
+    }
+  }
+  
+  class Running extends Workout {
+    type = 'running';
+  
+    constructor(coords, distance, duration, cadence) {
+      super(coords, distance, duration);
+      this.cadence = cadence;
+      this.calcPace();
+    }
+  
+    calcPace() {
+      // min/km
+      this.pace = this.duration / this.distance;
+      return this.pace;
+    }
+  }
+  
+  class Cycling extends Workout {
+    type = 'cycling';
+  
+    constructor(coords, distance, duration, elevationGain) {
+      super(coords, distance, duration);
+      this.elevationGain = elevationGain;
+      this.calcSpeed();
+    }
+  
+    calcSpeed() {
+      // km/h
+      this.speed = this.distance / (this.duration / 60);
+      return this.speed;
+    }
+  }
 
 class App {
     #map;
@@ -16,10 +57,7 @@ class App {
         this._getPosition();
 
         form.addEventListener('submit', this._newWorkout.bind(this))
-        inputType.addEventListener('change', function () {
-            inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
-            inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
-        })
+        inputType.addEventListener('change', this._toggleElevationField)
     }
 
     _getPosition() {
@@ -55,6 +93,11 @@ class App {
     _hideForm() {
 
     }
+
+    _toggleElevationField() {
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+      }
     _newWorkout(e) {
         e.preventDefault()
         inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
