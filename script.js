f4da6fdd2a15fea61ccefc6b9ce9c95a1d1dc408
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-03-20 22:37:22 +0530

    set description

--- script.js
+++ script.js
@@ -15,6 +15,19 @@ class Workout {
       this.distance = distance; // in km
       this.duration = duration; // in min
     }
+
+    _setDescription() {
+        // prettier-ignore
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    
+        this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
+          months[this.date.getMonth()]
+        } ${this.date.getDate()}`;
+      }
+    
+      click() {
+        this.clicks++;
+      }
   }
   
   class Running extends Workout {
@@ -24,6 +37,7 @@ class Workout {
       super(coords, distance, duration);
       this.cadence = cadence;
       this.calcPace();
+      this._setDescription();
     }
   
     calcPace() {
@@ -40,6 +54,7 @@ class Workout {
       super(coords, distance, duration);
       this.elevationGain = elevationGain;
       this.calcSpeed();
+      this._setDescription();
     }
   
     calcSpeed() {
